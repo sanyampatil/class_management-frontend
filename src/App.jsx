c2951801954221c8ba8d948cc80e237759973214
sanyam <patilsanyam869@gmail.com>
2024-01-13 09:39:32 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <div className=''>
+        <h1>hii i am sanyam</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
